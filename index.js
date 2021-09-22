bd9f02f92ec9805b7aa34abd51c7323a0ac575cc
Bang Nguyen Le Ly <bangjdev@gmail.com>
2021-09-23 01:02:21 +0200

    Implement command ping, add

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { prefix, token } = require('./config.json');
-const ytdl = require('ytdl-core');
+const dotenv = require('dotenv');
+const { Client, Intents } = require("discord.js");
+
+dotenv.config();
+
+const prefix = process.env.PREFIX;
+const token = process.env.TOKEN;
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
-
 const queue = new Map();
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", async () => {
+  console.log("Ready!");
 });
-client.once('reconnecting', () => {
-  console.log('Reconnecting!');
+
+client.once("reconnecting", () => {
+  console.log("Reconnecting!");
 });
-client.once('disconnect', () => {
-  console.log('Disconnect!');
+client.once("disconnect", () => {
+  console.log("Disconnect!");
 });
 
 // Login to Discord with your client's token
 client.login(token);
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) {
+    return;
+  }
+  const { commandName, options } = interaction;
+
+  switch (commandName) {
+    case "ping":
+      interaction.reply({
+        content: "PONG!",
+        ephemeral: true
+      });
+      break;
+    case "add":
+      const firstNumber = options.getNumber("number1") || 0;
+      const secondNumber = options.getNumber("number2") || 0;
+      const res = firstNumber + secondNumber;
+
+      interaction.reply({
+        content: res.toString(),
+        ephemeral: true
+      });
+
+      break;
+    default:
+      break;
+  }
+});
