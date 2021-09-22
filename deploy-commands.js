1a38e3a1360bfcd4baf1e05ab74777e6392ae1ca
Bang Nguyen Le Ly <bangjdev@gmail.com>
2021-09-23 00:50:23 +0200

    Implement commands deployer

---
+++ deploy-commands.js
@@ -0,0 +1,43 @@
+const { commandsList } = require("./commands");
+const { Client, Intents } = require("discord.js");
+
+require("dotenv").config();
+
+const token = process.env.TOKEN;
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
+
+client.once("ready", async () => {
+  console.log("Initializing commands handler...");
+
+  const guildId = process.env.GUILD_ID;
+  const guild = client.guilds.cache.get(guildId);
+
+  let commands;
+
+  if (guild) {
+    commands = guild.commands;
+  } else {
+    commands = client.application.commands;
+  }
+
+  for (const c of commandsList) {
+    await commands.create(c);
+    console.log(`Command ${c.name} deployed`);
+  }
+
+  console.log("Finish!");
+
+  process.exit();
+});
+
+client.once("reconnecting", () => {
+  console.log("Reconnecting!");
+});
+client.once("disconnect", () => {
+  console.log("Disconnect!");
+});
+
+// Login to Discord with your client"s token
+client.login(token);
