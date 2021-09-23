ccf2542d82a4c10adfa2b871b0340e2fcfb515dd
Bang Nguyen Le Ly <bangjdev@gmail.com>
2021-09-24 00:28:57 +0200

    [SEARCH] Listen to button click event

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const dotenv = require('dotenv');
 const { Client, Intents } = require("discord.js");
 const { executeSearch } = require('./command/search');
 const { SEARCH_COMMAND } = require('./utils/constants');
+const LastCommand = require("./utils/last-command");
 
 // Configuration
 dotenv.config();
 
 // Declaration
 const token = process.env.TOKEN;
+const lastCommand = new LastCommand();
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
@@ -32,6 +34,11 @@ client.login(token);
 client.on('interactionCreate', async (interaction) => {
 	if (interaction.isCommand()) {
 		handleCommand(interaction);
+		return;
+	}
+	if (interaction.isButton()) {
+		handleButton(interaction);
+		return;
 	}
 });
 
@@ -62,6 +69,32 @@ const handleCommand = async (interaction) => {
 				results: await executeSearch(interaction)
 			});
 		default:
+			break;
+	}
+}
+
+const handleButton = async (interaction) => {
+    const guildId = interaction.guildId;
+    const userId = interaction.userId;
+
+    await interaction.deferReply();
+
+	const { command, results } = lastCommand.get(guildId, userId);
+	
+	await interaction.message.delete();
+    // Need to check if the button clicked belongs to the same message as last command
+
+    switch (command) {
+		case SEARCH_COMMAND:
+			const songIndex = parseInt(interaction.customId);
+			interaction.editReply({
+				content: `Track **${results[songIndex].title}** added to the queue!`,
+			});
+			break;
+		default:
+			interaction.editReply({
+				content: `What?`,
+			});
 			break;
 	}
 }
