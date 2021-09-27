e1cff129eaf09a58aae4335ffe81feba4bf2444a
Bang Nguyen Le Ly <bangjdev@gmail.com>
2021-09-27 21:10:48 +0200

    Implement play method
    This method is dedicated to play after a search command.

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -1,122 +1,58 @@
-import * as ytdl from 'ytdl-core';
 
 import {
-  AudioPlayerStatus,
   VoiceConnectionStatus,
-  createAudioPlayer,
-  createAudioResource,
   entersState,
-  getVoiceConnection,
   joinVoiceChannel,
 } from '@discordjs/voice';
 
-import { Guild } from 'discord.js';
+import { GuildMember } from 'discord.js';
 import { InteractiveInteraction } from '@/model/interaction';
-import { SongQueue } from '@/utils/song-queue';
+import { QueueManager } from '@/utils/queue-manager';
+import { Container } from 'typedi';
+import { QueueObject } from '@/utils/queue-object';
+import { Track } from '@/utils/track';
 
-async function play(guild: Guild, queue: SongQueue) {
-  const song = queue.getSong(guild.id);
-  if (!song) {
-    console.log('Destroying queue');
-    queue.destroyQueue(guild.id);
-    console.log(queue.getQueue(guild.id));
-    return;
-  }
+const TIMEOUT = parseInt(process.env.timeout);
 
-  if (
-    queue
-      .getQueue(guild.id)
-      .connection.subscribe(queue.getQueue(guild.id).player)
-  ) {
-    const stream = ytdl(song.url, {
-      filter: 'audioonly',
-      quality: 'highestaudio',
-      highWaterMark: 1 << 25,
-    });
+export async function playAfterSearch(interaction: InteractiveInteraction, track: Track) {
+  const guild = interaction.guild;
 
-    const resource = createAudioResource(stream);
-    queue.getQueue(guild.id).player.play(resource);
-    queue.getQueue(guild.id).textChannel.send(`Now playing: **${song.title}**`);
+  const queueManager = Container.get(QueueManager);
+  let queue = queueManager.getQueue(guild.id);
 
-    try {
-      await entersState(
-        queue.getQueue(guild.id).player,
-        AudioPlayerStatus.Idle,
-        +process.env.timeout
+  // If a queue of the guild doesn't already exist
+  // and the user is in a voice channel, join that channel
+  // and create a queue.
+  if (!queue) {
+    if (interaction.member instanceof GuildMember && interaction.member.voice.channel) {
+      const channel = interaction.member.voice.channel;
+      queue = new QueueObject(
+        joinVoiceChannel({
+          channelId: channel.id,
+          guildId: channel.guild.id,
+          adapterCreator: channel.guild.voiceAdapterCreator,
+        }),
       );
-      play(guild, queue);
-    } catch (err) {
-      console.error(err);
+      queue.voiceConnection.on('error', console.warn);
+      queueManager.setQueue(interaction.guildId, queue);
     }
   }
-}
 
-export async function executePlay(
-  interaction: InteractiveInteraction,
-  queue: SongQueue,
-  songRequest = null
-) {
-  const guild = interaction.guild;
-  const member = guild.members.cache.get(interaction.member.user.id);
-  const voiceChannel = member.voice.channel;
-
-  if (!voiceChannel) {
-    return interaction.channel.send(
-      'You must be in a voice channel to play music!'
-    );
-  }
-
-  if (!queue.getQueue(guild.id)) {
-    const queueObject = {
-      textChannel: interaction.channel,
-      voiceChannel,
-      connection: null,
-      player: createAudioPlayer(),
-      songs: [],
-      playing: true,
-    };
-    queue.setQueue(guild.id, queueObject);
-  }
-
-  if (songRequest) {
-    queue.addSong(guild.id, songRequest);
-    await interaction.channel.send(
-      `Track **${songRequest.title}** added to the queue!`
-    );
-  }
-
-  connectAndPlay(guild, queue);
-}
-
-export function connectAndPlay(guild: Guild, queue: SongQueue) {
-  // Sanitize
-  if (!queue.getQueue(guild.id)) {
+  // If still doesn't exist => User is not in a channel
+  if (!queue) {
+    await interaction.editReply("You must be in a channel to play music!");
     return;
   }
 
-  const voiceChannel = queue.getQueue(guild.id).voiceChannel;
-  queue.getQueue(guild.id).connection = getVoiceConnection(
-    voiceChannel.guild.id
-  );
-  console.log('Connection ' + queue.getQueue(guild.id).connection);
-
-  if (!queue.getQueue(guild.id).connection) {
-    queue.getQueue(guild.id).connection = joinVoiceChannel({
-      channelId: voiceChannel.id,
-      guildId: guild.id,
-      adapterCreator: guild.voiceAdapterCreator,
-    });
-
-    queue
-      .getQueue(guild.id)
-      .connection.on(VoiceConnectionStatus.Disconnected, () => {
-        console.log('Bot disconnected');
-        queue.getQueue(guild.id).connection.destroy();
-      });
+  // Make sure the connection is ready before processing the user's request
+  try {
+    await entersState(queue.voiceConnection, VoiceConnectionStatus.Ready, TIMEOUT);
+  } catch (error) {
+    console.warn(error);
+    await interaction.editReply(`Failed to join voice channel within ${TIMEOUT}ms, please try again later!`);
+    return;
   }
-  if (
-    queue.getQueue(guild.id).player._state.status === AudioPlayerStatus.Idle
-  ) {
-    play(guild, queue);
-  }
+
+  queue.addTrack(track);
+  await interaction.editReply(`Track **${track.title}** added to the queue!`);
 }
